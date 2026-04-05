ebc1887a5963d37fb7ce5c3cac3de94472775004
ClawdBump <milosbrengsek@gmail.com>
2026-04-05 12:44:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # 1. Menyalakan Xvfb (Virtual Display) di background
-Xvfb :99 -screen 0 1366x768x24 &
+Xvfb :99 -screen 0 1366x1024x24 &
 sleep 2
 
 # 2. Tambahkan ini agar bot tahu layar mana yang digunakan
